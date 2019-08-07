cb3793528517acee615cb8d2458ae9ccaa97ea70
Julia Tokmagasheva <tokmagasheva@mail.ru>
2019-08-07 12:53:27 +0700

    added checking status code response

--- OVIntegration.py
+++ OVIntegration.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import sys
 
 from requests.auth import HTTPBasicAuth
 
@@ -34,9 +35,10 @@ class OVIntegration(object):
     def search_trackor(self):
         try:
             url = self.url + "/api/v3/trackor_types/" + str(self.trackorType) + "/trackors?Trackor_key=%22" + str(self.trackorKey) + "%22"
-            answer = requests.get(url, headers=self.headers, auth=self.auth)
-            response = answer.json()
-            return response
+            response = requests.get(url, headers=self.headers, auth=self.auth)
+            self.check_status_code(response.status_code, 'Search Trackor')
+            answer = response.json()
+            return answer
         except Exception as e:
             return ""
 
@@ -47,15 +49,32 @@ class OVIntegration(object):
                 "TRACKOR_KEY": str(self.trackorKey)
             }
         }
-        answer = requests.post(url, headers=self.headers, data=json.dumps(data), auth=self.auth)
-        response = answer.json()
+        response = requests.post(url, headers=self.headers, data=json.dumps(data), auth=self.auth)
+        self.check_status_code(response.status_code, 'Create Trackor')
+        answer = response.json()
         return response['TRACKOR_ID']
 
-    def add_log(self, message, description, log_level_name):
+    def add_log(self, message, description, log_level):
         url = self.url + "/api/v3/integrations/runs/logs/" + str(self.processId) + "/logs"
         data = {
             "message": message,
             "description": description,
-            "log_level_name": log_level_name
+            "log_level_name": log_level
         }
-        requests.post(url, headers=self.headers, data=json.dumps(data), auth=self.auth)
+        response = requests.post(url, headers=self.headers, data=json.dumps(data), auth=self.auth)
+        if response.status_code == 400 and log_level!='Error':
+                self.add_log(response.text, response.text, 'Error')
+        else:
+            self.check_status_code(response.status_code, 'Add Integration Log')
+
+    def check_status_code(self, status_code, call_method):
+        if status_code == 400:
+            sys.exit(call_method + ' - ' + 'Invalid JSON provided in request')
+        elif status_code == 401:
+            sys.exit(call_method + ' - ' + 'Authorization failed')
+        elif status_code == 403:
+            sys.exit(call_method + ' - ' + 'Calling user doesn\`t have permissions on certain objects or actions')
+        elif status_code == 404:
+            sys.exit(call_method + ' - ' + 'Some data required for processing is missed')
+        elif status_code == 500:
+            sys.exit(call_method + ' - ' + 'Any other error during request processing')
